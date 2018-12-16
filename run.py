b71135155c899e1ca4b1f557cbaa5374ed14d0c7
botdetective <45883204+botdetective@users.noreply.github.com>
2018-12-15 23:58:26 -0500

    Update run.py

--- run.py
+++ run.py
@@ -1,8 +1,7 @@
 import praw
 import config
-import urllib2
 import time
-import urllib
+import urllib.request, urllib.parse, urllib.error
 import os
 
 def bot_login():
@@ -11,10 +10,20 @@ def bot_login():
 				password = config.password,
 				client_id = config.client_id,
 				client_secret = config.client_secret,
-				user_agent = "Beep Boop Bot")
+				user_agent = "u/-WarHounds-'s anti product spam hunting bot that replies on comments by spammers to save innocent redditors from scams")
 	print("Logged in!")
 
 	return r
+
+def comments_rep_to():
+    with open ("comments_replied_to.txt", "a") as f:
+        f.write(comment.id + "\n")
+
+def checkop():
+    url = ("https://api.pushshift.io/reddit/search/comment/?author=" + comment.submission.author.name)
+    response = urllib2.urlopen(url)
+    with open('output.txt', 'w') as shtt:
+        shtt.write(response.read())
 
 def checkingvotes():
     print("Checking votes.")
@@ -22,23 +31,21 @@ def checkingvotes():
 def run_bot(r, comments_replied_to):
 	print("Searching last 1,000 comments")
 
-	for comment in r.subreddit('Damnthatsinteresting+test+DesignPorn+DidntKnowIWantedThat+Eyebleach+Perfectfit+Unexpected+aww+blackmagicfuckery+funny+geek+gifs+gifsthatkeepongiving+holdmybeer+interestingasfuck+oddlysatisfying+pics+videos+woahdude+combinedgifs+beamazed+nextfuckinglevel+wholesomegifs+noisygifs+ofcoursethatsathing+productporn+holdmycatnip+bettereveryloop+gifsthatendtoosoon+holdmycosmo+geek+yesyesyesno+yesyesyesyesno').comments(limit=1000):
+	for comment in r.subreddit('popular+mechanical_gifs+Damnthatsinteresting+test+DesignPorn+DidntKnowIWantedThat+Eyebleach+Perfectfit+Unexpected+aww+blackmagicfuckery+funny+geek+gifs+gifsthatkeepongiving+holdmybeer+interestingasfuck+oddlysatisfying+pics+videos+woahdude+combinedgifs+beamazed+nextfuckinglevel+wholesomegifs+noisygifs+ofcoursethatsathing+productporn+holdmycatnip+bettereveryloop+gifsthatendtoosoon+holdmycosmo+geek+yesyesyesno+yesyesyesyesno').comments(limit=1000):
 		if "/u/BotDetective test" in comment.body and comment.id not in comments_replied_to and comment.author or "etrendan.com/products/" in comment.body and comment.id not in comments_replied_to and comment.author or "geekydeal.store/products/" in comment.body and comment.id not in comments_replied_to and comment.author or "pearlgadget.com/products/" in comment.body and comment.id not in comments_replied_to and comment.author or "stiflingdeals.com/products/" in comment.body and comment.id not in comments_replied_to and comment.author or "prenkart.com/products/" in comment.body and comment.id not in comments_replied_to and comment.author or "kickize.com/products/" in comment.body and comment.id not in comments_replied_to and comment.author or "hashtagssale.com/products/" in comment.body and comment.id not in comments_replied_to and comment.author != r.user.me():
-			print("String with \"fishy link\" found in comment " + comment.id)
+			print(("String with blacklisted link found in comment " + comment.id))
 			comment.reply("**WARNING:** This is likely a fake account setup to promote the product shown in the video, please don't encourage this by either visiting the website or upvoting.\n\nThese bots are setup to steal unique comments from other users to pass as real people.\n\nIf you would like to help verify whether this user is a bot, you can check for duplicate comments using redditsearch.io\n\n**^If ^the ^user ^is ^indeed ^a ^bot, ^please ^report ^the ^message ^to ^the ^moderators, ^If ^you ^believe ^this ^was ^a ^mistake, [^send ^me ^a ^message!](https://www.reddit.com/message/compose/?to=-WarHounds-&subject=BotDetective - Error)**")
-			print("Replied to comment " + comment.id)
+			print(("Replied to comment " + comment.id))
 
 			comments_replied_to.append(comment.id)
-
-			with open ("comments_replied_to.txt", "a") as f, open('acc_history.txt', 'w') as c, open ("check_if_op_is_bot.txt", "w") as b:
-				f.write(comment.id + "\n")
-				b.write("https://api.pushshift.io/reddit/search/comment/?author=" + comment.submission.author.name + "\n")
-
+			comments_rep_to()
+			checkop()
+        
 	print("Search Completed.")
 
-	print(comments_replied_to)
-
 	get_karma()
+
+	print(comments_replied_to)
 
 	print("Sleeping for 60 seconds...")
 	#Sleep for 10 seconds...
@@ -52,9 +59,9 @@ def get_karma():
     p_id = open("Parent_id.txt","a+")
     for comment in comment_list:
         comment = r.comment(id = comment)
-        print("Comment with " + str(comment.score) + " found")
+        print(("Comment with " + str(comment.score) + " found"))
         if int(comment.score) < 0:
-            print("Comment with " + str(comment.score) + " found")
+            print(("Comment with " + str(comment.score) + " found"))
             parent_id = comment.parent_id
             if parent_id not in p_id.read().split("\n"):
                 parent_comment = r.comment(id=parent_id)
@@ -65,7 +72,7 @@ def get_karma():
                 print("Comment reposted.")
         else:
             checkingvotes()
-	
+
 def get_saved_comments():
 	if not os.path.isfile("comments_replied_to.txt"):
 		comments_replied_to = []
@@ -73,7 +80,7 @@ def get_saved_comments():
 		with open("comments_replied_to.txt", "r") as f:
 			comments_replied_to = f.read()
 			comments_replied_to = comments_replied_to.split("\n")
-			comments_replied_to = filter(None, comments_replied_to)
+			comments_replied_to = [_f for _f in comments_replied_to if _f]
 
 	return comments_replied_to
 
